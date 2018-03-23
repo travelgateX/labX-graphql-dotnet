43a58c5b361c681eee4c32277e2bcedd86c547f4
rcaballero <rcaballero@xmltravelgate.com>
2018-03-23 09:24:26 +0100

    Set performance

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace Example
 {
@@ -8,15 +10,32 @@ namespace Example
     {
         public static void Main(string[] args)
         {
-            var directory = Directory.GetCurrentDirectory();
+            IWebHost host = WebHost.CreateDefaultBuilder(args)
+                 .UseContentRoot(System.IO.Directory.GetCurrentDirectory())
+                 .UseUrls("http://*:80") // -> Must match the Kestrel configuration.
+                 .UseStartup<Startup>()
+                 .UseLibuv(options =>
+                 {
+                     options.ThreadCount = 32;
+                 })
+                 .UseKestrel(options =>
+                 {
+                     options.Limits.KeepAliveTimeout = TimeSpan.FromSeconds(60);
 
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(directory)
-                .UseWebRoot(Path.Combine(directory, "public"))
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+                     options.ApplicationSchedulingMode = Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal.SchedulingMode.ThreadPool;
+                     options.Limits.MinRequestBodyDataRate =
+                         new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                     options.Limits.MinResponseDataRate =
+                         new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                     options.Limits.MaxConcurrentConnections = null;             // No limit. Default: null
+                     options.Limits.MaxConcurrentUpgradedConnections = null;     // No limit. Default: null
+                                                                                 //--- Kestrel endpoints -----------------
+                     options.Listen(IPAddress.Any, 80, listenOptions =>
+                     {
+                         listenOptions.NoDelay = true;
+                     });
+                 })
+                 .Build();
 
             host.Run();
         }
---
+++ src/utils/ClientHTTP.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace starwars.utils
+{
+    public class ClientHTTP
+    {
+        public async Task<bool> httpClient()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.Proxy = null;
+            handler.UseProxy = false;
+            handler.AutomaticDecompression = System.Net.DecompressionMethods.GZip;
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+
+                await Task.Delay(0);
+                //LLamada
+                //HttpResponseMessage resp = await cliente.PostAsync(....);
+            }
+
+            return true;
+        }
+
+    }
+}
