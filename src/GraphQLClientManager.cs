7d392d32bd133036ac292e26f52606961ea70436
cacedo <cacedo@xmltravelgate.com>
2018-03-23 09:50:52 +0100

    add client manager

---
+++ src/GraphQLClientManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace starwars
+{
+    public class GraphQLClientManager
+    {
+        private static readonly object SYNCLOCK_LOCK = new object();
+
+        private static GraphQLClientManager instance;
+
+        private GraphQLClientManager()
+        {
+            //TODO : create http
+
+        }
+
+        public static GraphQLClientManager getInstance()
+        {
+            if (GraphQLClientManager.instance == null)
+            {
+                lock (SYNCLOCK_LOCK)
+                {
+                    if (GraphQLClientManager.instance == null)
+                    {
+                        GraphQLClientManager.instance = new GraphQLClientManager();
+                    }
+                }
+            }
+            return GraphQLClientManager.instance;
+        }
+
+
+    }
+}
